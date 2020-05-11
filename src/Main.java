4e907f5e487529c09434ee402d6198248ae98d9a
MemphisCornelius <theeasyminer@gmail.com>
2020-05-11 18:39:41 +0200

    Output in extra datei

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Main {
 
+
     public static void main(String[] args) {
-        new Simualtion(1,1,1);
+
+        try {
+            PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
+            System.setOut(out);
+        } catch (FileNotFoundException e) { e.printStackTrace(); }
+
+        LocalDateTime start = LocalDateTime.now();
+        new Simualtion(10,2,10);
+        System.out.println(Duration.between(start, LocalDateTime.now()).toMillis() + "ms");
     }
 }
